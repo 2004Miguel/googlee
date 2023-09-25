0fd32e912d3c8b20f03c92a51d4cf388b34dfb06
2004Miguel <miguesalas2004@gmail.com>
2023-09-25 18:56:41 -0500

    avance del primer botón
    intento del commit. Llevamos la lógica del primer botón (el de ir a google)

--- app/src/main/java/com/Miguel/correo/MainActivity.java
+++ app/src/main/java/com/Miguel/correo/MainActivity.java
@@ -2,13 +2,49 @@ package com.Miguel.correo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button google, alarma, llamada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        google = findViewById(R.id.ir_google);
+        alarma = findViewById(R.id.go_alarma);
+        llamada = findViewById(R.id.go_llamada);
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent irAGoogle=new Intent(Intent.ACTION_VIEW);
+                irAGoogle.setData(Uri.parse("https://www.google.com"));
+
+            }
+        });
+
+        alarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        llamada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
