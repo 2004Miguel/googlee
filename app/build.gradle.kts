d604e3a02292a575c0642712876594cd42b81bd4
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2023-09-30 11:44:40 -0500

    El botón de abrir la cámara funciona

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,11 +22,21 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures {
+        viewBinding;true
+    }
+
 }
+
+
+
+
 
 dependencies {
 
@@ -36,4 +46,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
+
+dependencies {
+    //def camerax_version = "1.1.0-beta01" dependecias para abrir la camara
+    implementation ("androidx.camera:camera-core:1.1.0-beta01")
+    implementation ("androidx.camera:camera-camera2:1.1.0-beta01")
+    implementation ("androidx.camera:camera-lifecycle:1.1.0-beta01")
+    implementation ("androidx.camera:camera-video:1.1.0-beta01")
+
+    implementation ("androidx.camera:camera-view:1.1.0-beta01")
+    implementation ("androidx.camera:camera-extensions:1.1.0-beta01")
+}
+
--- app/src/main/java/com/Miguel/correo/MainActivity.java
+++ app/src/main/java/com/Miguel/correo/MainActivity.java
@@ -1,17 +1,26 @@
 package com.Miguel.correo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Presentation;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageCapture;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button google, alarma, llamada;
+
+    Button google, alarma, llamada, aactivity2, caamara;
+    ImageView visor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,26 @@ public class MainActivity extends AppCompatActivity {
         google = findViewById(R.id.ir_google);
         alarma = findViewById(R.id.go_alarma);
         llamada = findViewById(R.id.go_llamada);
+        aactivity2 = findViewById(R.id.activity2);
+        caamara = findViewById(R.id.foto);
+
+        caamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                camara();
+            }
+        });
+
+        aactivity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent enviar_datos = new Intent(MainActivity.this, Activity2.class);
+
+                startActivity(enviar_datos);
+
+            }
+        });
 
         google.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,7 +89,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        caamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                camara();
 
+            }
+        });
+
+
+    }
+
+    private void camara() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(intent, 1);
+
+            //para que funcione se agregaron estos permisos en el manifest
+            //<uses-feature android:name="android.hardware.camera"
+            //android:required="true">
+            //</uses-feature>
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resulCode, Intent data){
+        super.onActivityResult(requestCode, requestCode, data);
+        if(requestCode == 1 && resulCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            visor.setImageBitmap(imgBitmap);
+        }
     }
 
     private void crearAlarma() {
